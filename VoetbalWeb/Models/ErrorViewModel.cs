18c3bd8686b24168a11c14ce5615197022a77e49
dhoin <tom.dhoine@student.vives.be>
2024-03-11 11:24:02 +0100

    first commit

---
+++ VoetbalWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VoetbalWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
